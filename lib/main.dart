47d50a2914f07914ee756deceefee4d4139c70cd
susiyaki <susiyaki.dev@gmail.com>
2020-08-12 17:30:56 +0900

    add fetchTodo func

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/widgets/home/actions.dart';
+import 'package:todo_app/screens/top.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo App',
       theme: ThemeData(
         primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Todo Lists'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title, style: TextStyle(color: Colors.white)),
-      ),
-      body: Column(
-        children: [
-          HomeActions(),
-        ],
-      ),
+      home: MyTopPage(),
     );
   }
 }
